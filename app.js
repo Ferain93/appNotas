450bd7e852872029fc466b2cf3d14d3d8f735e72
Ferain93 <fer.tn7@gmail.com>
2021-09-24 21:44:38 -0500

    bug: case crear

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const { escribir, leer } = require('./tareas');
+//const { escribir, leer  = require('./tareas');
 let archivoTareas = require('./tareas');
 
 let accion = process.argv[2];
+let titulo = process.argv[3];
+let estado = process.argv[4];
 
 
 switch(accion) {
     case 'listar':
         console.log('Listado de tareas esta dado por: ');
-        const tareas = leer();
+        const tareas = archivoTareas.leer();
         tareas.forEach(function(element){
             console.log("El titulo de la tarea es " + element.titulo + " y el estado es " + element.estado)
         });
@@ -18,23 +20,14 @@ switch(accion) {
         console.log('Tenés que pasarme una acción');
         break;
 
-    // Micro desafío 2
-    case 'escribir':
+    case 'crear':
         console.log('Acción Leer');
-        
-        let task = ["Titulo" , "Estado"];
-        let newtask =[];
-        let titulo;
-        let estado;
-
-        newtask.push(`Ingrese un ${task[0]} : ${process.argv[3]}`);
-        newtask.push(`Ingrese un ${task[1]} : ${process.argv[4]}`);
-        
-        console.log(newtask);
+        const tareas = archivoTareas.guardarTarea(titulo,estado);
+        console.log('Tarea Guardada');
         break;
 
     default:
         console.log('No entiendo qué me estás pidiendo');
-        console.log('Las acciones disponibles son: listar, crear y buscar');
+        console.log('Las acciones disponibles son: listar, crear y filtar');
         break;
 }
--- tareas.js
+++ tareas.js
@@ -7,8 +7,15 @@ let archivoTareas = {
     leer: function () {
         return JSON.parse(fs.readFileSync('tareas.json', 'utf-8'));
     },
-    escribir: function () {
-        return JSON.parse(fs.writeFileSync('tareas.json', 'utf-8'));
+    guardarTarea: function(titulo,estado) {
+        const tareas= {
+            titulo : titulo,
+            estado : estado
+        }
+        const database = logic
+        database.push(tareas)
+        const tareaJson = JSON.stringify(database,null,4)
+        fs.writeFileSync("./tareas.json",tareaJson)
     },
 }
 
